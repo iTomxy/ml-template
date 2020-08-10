4db8aa88f3f96026ff4c273d0f69645f489ebcf5
iTom <44149820+iTomxy@users.noreply.github.com>
2020-08-10 18:10:25 +0800

    udpate log in PyTorch's main.py

--- pytorch/main.py
+++ pytorch/main.py
@@ -12,10 +12,10 @@ from tool import *
 from args import *
 
 
-print(torch.__version__)               # PyTorch version
-print(torch.version.cuda)              # Corresponding CUDA version
-print(torch.backends.cudnn.version())  # Corresponding cuDNN version
-print(torch.cuda.get_device_name(0))   # GPU type
+print("PyTorch version:", torch.__version__)
+print("cuda version:", torch.version.cuda)
+print("cuDNN version:", torch.backends.cudnn.version())
+print("GPU type:", torch.cuda.get_device_name(0))
 
 
 logger = Logger(args)
