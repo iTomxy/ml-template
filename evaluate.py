dede75cca1b204ffafbbb8e86035f41cc0730b03
iTom <44149820+iTomxy@users.noreply.github.com>
2020-11-10 10:26:32 +0800

    fix a trivial bug of NDCG_tie

--- evaluate.py
+++ evaluate.py
@@ -135,23 +135,26 @@ def WAP(Dist, Rel, k=-1):
     return _WAP / n
 
 
-def mAHP(Dist, S, k=-1):
+def mAHP(Dist, Rel, k=-1):
     """mean Average Hierarchical Precision
-    S: similarity matric, CAN be multi-level
-    ref: Hierarchical Semantic Indexing for Large Scale Image Retrieval
+    APH@k = 1/k * [ sum_i {HP@i} - 1/2 * (HP@1 + HP@k) ]
+    ref:
+    - https://github.com/cvjena/semantic-embeddings/issues/4
+    - Hierarchical Semantic Indexing for Large Scale Image Retrieval
     """
     n, m = Dist.shape
     if (k < 0) or (k > m):
         k = m
     Rank = np.argsort(Dist)
 
-    APH = 0
-    for s, rnk in zip(S, Rank):
-        aph_best = np.cumsum(np.sort(s)[::-1][:k])
-        if aph_best[0] > 0:
-            aph_real = np.cumsum(s[rnk[:k]])
-            APH += (aph_real / aph_best).mean()
-    return APH / n
+    AHP = 0
+    for rel, rnk in zip(Rel, Rank):
+        sim_sum_best = np.cumsum(np.sort(rel)[::-1][:k])
+        if sim_sum_best[0] > 0:
+            sim_sum_real = np.cumsum(rel[rnk[:k]])
+            hp = sim_sum_real / sim_sum_best
+            AHP += hp.mean() - 0.5 * (hp[0] + hp[-1]) / k
+    return AHP / n
 
 
 def ap_pc(y_true, y_score):
@@ -338,10 +341,63 @@ def NDCG_tie(Dist, Rel, k=-1):
             tc = tie_pos[-1]
             sum_d[tie_idx] = (1 / np.log2(1 + np.arange(tc_1 + 1, min(k, tc) + 1))).sum()
 
-        dcg = ((g_sort / n_c) * sum_d)[:k].sum()
+        dcg = ((g_sort[:k] / n_c[:k]) * sum_d[:k]).sum()
         _NDCG += dcg / dcg_best
 
     return _NDCG / n
+
+
+def HP_tie(Dist, Rel, k=-1):
+    """get tie-aware HP list for mAHP_tie
+    return:
+    - M: [n, k], with M[i][j] = t-HP@j(q_i, V)
+    """
+    n, m = Dist.shape
+    if (k < 0) or (k > m):
+        k = m
+    Rank = np.argsort(Dist)
+    pos = 1 + np.arange(m)  # 1-base
+
+    t_hp_list = []
+    for dis, rnk, rel in zip(Dist, Rel, Rank):
+        sim_sum_best = np.cumsum(np.sort(rel)[::-1])  # [m]
+        d_sort = dis[rnk]  # [m]
+        d_unique = np.unique(dis)  # ascending
+        # sim_sum_pre[i]: sim sum of the previous ties that
+        #       before the tie where i-th elem lies
+        sim_sum_pre = np.zeros_like(dis)
+        _pre_sum = 0
+        # sim_sum_tie[i]: sim sum of the tie where i-th elem lies
+        sim_sum_tie = np.zeros_like(dis)
+        # tie_n[i]: #samples in the tie where i-th elem lies
+        tie_n = np.zeros_like(dis)
+        # t_{c-1}[i]: position before the 1st elem of the tie
+        #    where the i-th sample lies
+        tc_1 = np.zeros_like(dis)
+        for _d in d_unique:
+            mask_tie = (_d == dis)
+            tc_1[mask_tie] = pos[_d == d_sort][0] - 1
+            tie_n[mask_tie] = mask_tie.astype(np.int).sum()
+            _ss_tie = rel[mask_tie].sum()
+            sim_sum_tie[mask_tie] = _ss_tie
+            sim_sum_pre[mask_tie] = _pre_sum
+            _pre_sum += _ss_tie
+        t_hp = (sim_sum_pre + (pos - tc_1) * (sim_sum_tie / tie_n)) / sim_sum_best
+        t_hp_list.append(t_hp[:k])
+    return np.vstack(t_hp_list)
+
+
+def mAHP_tie(Dist, Rel, k=-1):
+    """mean Average Hierarchical Precision
+    ref: https://blog.csdn.net/HackerTom/article/details/107458334
+    """
+    t_HP = HP_tie(Dist, Rel, k)
+    n, m = Dist.shape
+    if (k < 0) or (k > m):
+        k = m
+    assert k == t_HP.shape[1]
+    AHP = t_HP.mean(1) - 0.5 * (t_HP[:, 0] + t_HP[:, -1]) / k
+    return AHP.mean()
 
 
 def t_sne(F, L, title="tsne"):
