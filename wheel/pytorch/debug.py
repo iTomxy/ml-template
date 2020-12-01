11cb9487001ce0d7ec22587c6a3292d536f1b5fe
iTom <44149820+iTomxy@users.noreply.github.com>
2020-12-01 20:41:11 +0800

    add pytorch hook

--- wheel/pytorch/debug.py
+++ wheel/pytorch/debug.py
@@ -16,3 +16,50 @@ def check_nan_inf(tensors):
     else:
         raise Exception("check_nan_inf: unsupported type: {}".format(type(tensors)))
     return 0
+
+
+def forward_nan(m, x, y):
+    if not isinstance(x, tuple):
+        x = [x]
+    if not isinstance(y, tuple):
+        y = [y]
+    for i, _x in enumerate(x):
+        if isinstance(_x, torch.Tensor) and torch.isnan(_x).any():
+            # print("{} output NaN".format(m.__class__.__name__))
+            raise RuntimeError("{}: {}-th input NaN".format(m.__class__.__name__, i))
+    for i, _y in enumerate(y):
+        if isinstance(_y, torch.Tensor) and torch.isnan(_y).any():
+            # print("{} output NaN".format(m.__class__.__name__))
+            raise RuntimeError("{}: {}-th output NaN".format(m.__class__.__name__, i))
+
+
+def backward_nan(m, dy, dy_dx):
+    if not isinstance(dy, tuple):
+        dy = [dy]
+    if not isinstance(dy_dx, tuple):
+        dy_dx = [dy_dx]
+    for i, _dy in enumerate(dy):
+        if isinstance(_dy, torch.Tensor):
+            # _pn = _dy.grad
+            # if _pn is not None:
+            #     _pn = _pn.data.norm(2).cpu().item()
+            # print("{} grad {}:".format(m.__class__.__name__, i), _pn)
+            if torch.isnan(_dy).any():
+                # print("{} output NaN".format(m.__class__.__name__))
+                raise RuntimeError("{}: {}-th input grad dy NaN".format(m.__class__.__name__, i))
+    for i, _dy_dx in enumerate(dy_dx):
+        if isinstance(_dy_dx, torch.Tensor) and torch.isnan(_dy_dx).any():
+            # print("{} output NaN".format(m.__class__.__name__))
+            raise RuntimeError("{}: {}-th output grad dy NaN".format(m.__class__.__name__, i))
+
+
+def hook_it(model):
+    for _m in model.modules():
+        # if _m.__class__.__name__ not in ("TALR", "AlexNet", "Sequential"):
+        # print(_m.__class__.__name__)
+        # _m.register_forward_hook(forward_nan)
+        # _m.register_backward_hook(backward_nan)
+        name_list = []
+        for name, p in _m.named_parameters():
+            name_list.append(name)
+        print(_m.__class__.__name__, ':', name_list)
