87ecf15ca686ee024e4192df613815303dfff236
iTom <44149820+iTomxy@users.noreply.github.com>
2020-12-10 16:12:21 +0800

    fix a stupid bug of load_images.m

--- +itom/load_images.m
+++ +itom/load_images.m
@@ -1,8 +1,10 @@
-function image_batch = load_images(path, indices, image_size)
-    image_path = {};
+function image_batch = load_images(image_path, indices, image_size)
+    image_files = {};
     for i = 1 : length(indices)
-        image_path(i) = {char(fullfile(path, sprintf("%d.jpg", i)))};
+        idx = indices(i);
+        image_files(i) = {char(fullfile(image_path, sprintf("%d.jpg", idx)))};
     end
-    img = vl_imreadjpeg(image_path, 'NumThreads', 4, 'Resize', [image_size, image_size], 'Pack');
+    img = vl_imreadjpeg(image_path, 'NumThreads', 4, ...
+        'Resize', [image_size, image_size], 'Pack', 'Interpolation', 'bilinear');
     image_batch = img{1, 1};  % (H, W, C, n)
 end
