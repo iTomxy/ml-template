2bb5aa915323e6f4099f8dddb41e391a458584bd
iTom <44149820+iTomxy@users.noreply.github.com>
2020-12-16 11:12:17 +0800

    fix bug

--- +itom/load_images.m
+++ +itom/load_images.m
@@ -4,7 +4,7 @@ function image_batch = load_images(image_path, indices, image_size)
         idx = indices(i);
         image_files(i) = {char(fullfile(image_path, sprintf("%d.jpg", idx)))};
     end
-    img = vl_imreadjpeg(image_path, 'NumThreads', 4, ...
+    img = vl_imreadjpeg(image_files, 'NumThreads', 4, ...
         'Resize', [image_size, image_size], 'Pack', 'Interpolation', 'bilinear');
     image_batch = img{1, 1};  % (H, W, C, n)
 end
