da7447e2c2681521a98d33d55e7d6a379b40f236
iTom <44149820+iTomxy@users.noreply.github.com>
2020-12-16 21:23:18 +0800

    update run.sh sample

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 # clear
 
 echo "hello world!"
-python main.py --data_path ~/data/data20371/ --cnnf_weight ~/data/data20371/vgg_net.mat
+# python main.py --data_path ~/data/data20371/ --cnnf_weight ~/data/data20371/vgg_net.mat
 
-# for i_fold in 0 1 2 3 4; do
-#     python main.py --data_path ~/data/data20371/ --cnnf_weight ~/data/data20371/vgg_net.mat --i_fold $i_fold --n_fold $n_fold
-#     if [ $? -ne 0 ]; then exit; fi
-# done
+n_fold=5
+for i_fold in $(seq 0 `expr $n_fold - 1`); do
+    python main.py --data_path ~/data/data20371/ --cnnf_weight ~/data/data20371/vgg_net.mat --i_fold $i_fold --n_fold $n_fold
+    if [ $? -ne 0 ]; then exit; fi
+done
