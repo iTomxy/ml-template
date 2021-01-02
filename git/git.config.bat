3591d6240f42dcd616d347cd6ffd9138decf8f11
iTom <44149820+iTomxy@users.noreply.github.com>
2021-01-02 10:07:21 +0800

    set `git push.default` to `simple`

--- git/git.config.bat
+++ git/git.config.bat
@@ -3,3 +3,5 @@
 REM init git repo
 git config user.name "iTom"
 git config user.email "itom@example.com"
+
+git config push.default simple
--- git/git.config.sh
+++ git/git.config.sh
@@ -3,3 +3,5 @@
 # init git repo
 git config user.name "iTom"
 git config user.email "itom@example.com"
+
+git config push.default simple
