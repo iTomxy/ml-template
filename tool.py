8bfac10f426e8f6df79eced71bcdc93ce65bc11a
iTom <44149820+iTomxy@users.noreply.github.com>
2021-05-12 21:13:27 +0800

    optional suppression of on-screen printing

--- tool.py
+++ tool.py
@@ -26,10 +26,11 @@ class Logger:
             self.log_file.close()
             # self.log_file = None
 
-    def __call__(self, text):
+    def __call__(self, text, on_screen=True):
         if self.log_file is None:
             self.open()
-        print(text)
+        if on_screen:
+            print(text)
         self.log_file.write(text + '\n')
 
     def open(self):
