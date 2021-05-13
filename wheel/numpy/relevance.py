138ad5f5df5f3aee4b00816559ea8ba0731bd9c0
iTom <44149820+iTomxy@users.noreply.github.com>
2021-05-13 16:05:35 +0800

    enhance readiblility

--- wheel/numpy/relevance.py
+++ wheel/numpy/relevance.py
@@ -5,7 +5,7 @@ def sim_mat(label, label_2=None, sparse=False):
     if label_2 is None:
         label_2 = label
     if sparse:
-        S = label[:, np.newaxis] == label_2[np.newaxis, :]
+        S = (label[:, np.newaxis] == label_2[np.newaxis, :])
     else:
         S = np.dot(label, label_2.T) > 0
     return S.astype(label.dtype)
