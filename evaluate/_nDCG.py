02d50d13c1715c769d9c2c7a8e6f37454f0ec693
iTom <44149820+iTomxy@users.noreply.github.com>
2021-10-24 20:38:31 +0800

    upgrade nDCG_tie: add multiple k support

--- evaluate/_nDCG.py
+++ evaluate/_nDCG.py
@@ -49,40 +49,67 @@ def nDCG(Dist, Rel, k=-1):
 
 def nDCG_tie(Dist, Rel, k=-1):
     """tie-aware NDCG
-    Dist: [n, m], Hamming distance matrix
+    Input:
+        Dist: [n, m], Hamming distance matrix
+        Rel: [n, m], relevance mattrix, in {0, 1, 2, ...}
+        k: nDCG@k, int or int tuple/list
+           default `-1` means nDCG@ALL
     ref:
     - https://blog.csdn.net/HackerTom/article/details/107458334
     - https://github.com/kunhe/TALR/blob/master/%2Beval/tieNDCG.m
     """
+    if isinstance(k, int):
+        k = [k]
+    else:
+        k = sorted(k)  # ascending
     n, m = Dist.shape
-    if (k < 0) or (k > m):
-        k = m
+    for kid in range(len(k)):
+        if (k[kid] < 0) or (k[kid] > m):
+            k[kid] = m
     G = 2 ** Rel - 1
     pos = np.arange(m) + 1  # 1-base
     D_inv = 1 / np.log2(1 + pos)
-    Rank = np.argsort(Dist)
+    Rank = np.argsort(Dist, axis=-1)
 
-    _NDCG = 0
-    n_c = np.zeros([m])
-    sum_d = np.zeros([m])
-    for g, d, rnk in zip(G, Dist, Rank):
-        dcg_best = (np.sort(g)[::-1] * D_inv)[:k].sum()
-        if 0 == dcg_best:
+    _nDCG = np.zeros([len(k)], dtype=np.float32)
+    for g, dist, rnk in zip(G, Dist, Rank):
+        g_desc = np.sort(g)[::-1]
+        if 0 == g_desc[0]:  # biggist DCG
             continue
-        d_unique = np.unique(d)  # ascending
-        d_sort = d[rnk]
+        dcg_best_list = (g_desc * D_inv).cumsum()
+        dist_unique = np.unique(dist)  # ascending
+        dist_sort = dist[rnk]
         g_sort = g[rnk]
-        for _d in d_unique:
-            tie_idx = (d_sort == _d)
+
+        _start_kid = 0
+        dcg_list = np.zeros([len(k)], dtype=np.float32)
+        for _d in dist_unique:
+            tie_idx = (dist_sort == _d)
             tie_pos = pos[tie_idx]
             tc_1 = tie_pos[0] - 1  # i.e. tie_pos.min() - 1
-            if tc_1 >= k:  # k <= t_{c-1} < t_c, out of range
-                break  # continue
-            n_c[tie_idx] = tie_idx.astype(np.int).sum()
             tc = tie_pos[-1]
-            sum_d[tie_idx] = (1 / np.log2(1 + np.arange(tc_1 + 1, min(k, tc) + 1))).sum()
+            assert tc > tc_1
+            while _start_kid < len(k):
+                if k[_start_kid] > tc_1:
+                    break
+                else:
+                    _start_kid += 1
+            if _start_kid >= len(k):
+                break
+            n_c = tie_idx.sum()
+            tie_avg_gain = g_sort[tie_idx].sum() / n_c  # != g_sort[tie_idx].mean()
+            tie_sum_d_list = (1 / np.log2(1 + np.arange(tc_1 + 1, tc + 1))).cumsum()
+            for kid_offset, _k in enumerate(k[_start_kid:]):
+                tie_sum_d = tie_sum_d_list[min(_k, tc) - tc_1 - 1]
+                tie_dcg = tie_avg_gain * tie_sum_d
+                dcg_list[kid_offset + _start_kid] += tie_dcg
 
-        dcg = ((g_sort[:k] / n_c[:k]) * sum_d[:k]).sum()
-        _NDCG += dcg / dcg_best
+        for kid, _k in enumerate(k):
+            if _k > 0:
+                _ndcg = dcg_list[kid] / dcg_best_list[_k - 1]  # 0-base
+                _nDCG[kid] += _ndcg
 
-    return _NDCG / n
+    _nDCG /= n
+    if 1 == _nDCG.shape[0]:
+        _nDCG = _nDCG[0]
+    return _nDCG
