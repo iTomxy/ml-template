b68b9a4461c053b42ac720cda9079631ccd90223
iTom <44149820+iTomxy@users.noreply.github.com>
2022-06-20 20:40:59 +0800

    mod image loading API mod meanpix calculation procedure add gensim to pt1.4.df

--- +itom/calc_meanpix.m
+++ +itom/calc_meanpix.m
@@ -1,3 +1,6 @@
+% image_path: str/char, assuming that file names are in format `<index>.jpg`
+% indices: 1-base int vector
+% image_size: int or [int int]
 function meanpix = calc_meanpix(image_path, indices, image_size)
     meanpix = single(zeros(image_size, image_size, 3));
     batch_size = 256;
@@ -5,7 +8,12 @@ function meanpix = calc_meanpix(image_path, indices, image_size)
     for i = 1 : batch_size : n
         meta_index = i : min(i + batch_size - 1, n);
         index = indices(meta_index);
-        image_batch = single(itom.load_images(image_path, index, image_size));  % [H, W, C, n]
+        img_files = {};
+        for k = 1 : length(index)
+            idx = index(k);
+            img_files(k) = {char(fullfile(image_path, sprintf("%d.jpg", idx)))};
+        end
+        image_batch = single(itom.load_images(img_files, image_size));  % [H, W, C, n]
         meanpix = meanpix + sum(image_batch, 4);
         fprintf("%d\n", i);
     end
--- +itom/load_images.m
+++ +itom/load_images.m
@@ -9,18 +9,18 @@
 %     image_batch = img{1, 1};  % (H, W, C, n)
 % end
 
-function image_batch = load_images(indices, id2img, image_size)
-% (2021.11.13) new image loading API
-% indices: [n], 1-base index vector
-% id2img: cell, maps index to corresponding absolute image file path
-% image_size: int
-%-------------------------------------------------------------------
-    image_files = {};
-    for i = 1 : length(indices)
-        idx = indices(i);
-        image_files(i) = {char(id2img{idx})};
-    end
+%% (2022.6.18) new image loading API
+% image_files: cell, paths of images to load
+% image_size: int or [int int]
+function image_batch = load_images(image_files, image_size)
+    if isscalar(image_size)
+        image_size = [image_size, image_size];
+    else
+        assert(isvector(image_size), "* Should be scalar or 2D vector");
+        assert(length(image_size) == 2, ...
+            sprintf("* dimension error: [%s]", num2str(size(image_size))));
+    end % if scalar
     img = vl_imreadjpeg(image_files, 'NumThreads', 4, ...
-        'Resize', [image_size, image_size], 'Pack', 'Interpolation', 'bilinear');
+        'Resize', image_size, 'Pack', 'Interpolation', 'bilinear');
     image_batch = img{1, 1};  % (H, W, C, n)
 end
