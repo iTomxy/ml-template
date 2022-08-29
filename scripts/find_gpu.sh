d9c82566d30afca5f9446b31109d5804b11db03a
iTom <44149820+iTomxy@users.noreply.github.com>
2022-08-29 11:20:21 +0800

    find_gpu.sh: default=1, `-1` indicates all

--- scripts/find_gpu.sh
+++ scripts/find_gpu.sh
@@ -3,7 +3,9 @@
 ## Calling
 # . <PATH PREFIX>/find_gpu.sh
 # . <PATH PREFIX>/find_gpu.sh 5
+# . <PATH PREFIX>/find_gpu.sh -1
 # . <PATH PREFIX>/find_gpu.sh 3 8000
+# . <PATH PREFIX>/find_gpu.sh -1 20480
 ## Input
 # $1: # of GPU needed
 # $2: lower bound of free memory needed per GPU (in MiB)
@@ -13,6 +15,14 @@
 # n_gpu_found: int, # of selected GPU.
 ## Reference
 # https://blog.csdn.net/HackerTom/article/details/126257508
+
+# 1st arg: # of GPU needed, default = 1
+#   <0 means requiring all GPUs.
+n_gpu_req=${1-"1"}
+# 2nd arg: lower bound of free memory needed per GPU (in MiB), default = 0
+#   i.e. only GPUs with free memory >= $2 will be selected.
+mem_lb=${2-"0"}
+# echo "Requiring ${n_gpu_req} GPUs with at least ${mem_lb}MB free memory"
 
 res=$(nvidia-smi | \
 	grep -E "[0-9]+MiB\s*/\s*[0-9]+MiB" | \
@@ -24,12 +34,11 @@ i=0
 res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
 	sort -n -k 2 -r))
 
-# 1st arg: # of GPU needed, default = # of all GPUs
-n_gpu_req=${1-`expr ${#res[@]} / 2`}
-# 2nd arg: lower bound of free memory needed per GPU (in MiB), default = 0
-#   i.e. only GPUs with free memory >= $2 will be selected.
-mem_lb=${2-"0"}
-# echo "Requiring ${n_gpu_req} GPUs with at least ${mem_lb}MB free memory"
+if [ ${n_gpu_req} -lt 0 ]; then
+	# echo using all GPUs
+	n_gpu_req=`expr ${#res[@]} / 2`
+	# echo ${n_gpu_req}
+fi
 
 gpu_id=-1
 n_gpu_found=0
