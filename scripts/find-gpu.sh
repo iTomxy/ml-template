f89ca028080cb1e0c4ff4a04d5244badc6852676
iTom <44149820+iTomxy@users.noreply.github.com>
2023-04-20 20:12:36 +0800

    support & default to best fit

--- scripts/find-gpu.sh
+++ scripts/find-gpu.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 ## Calling
-# . <PATH PREFIX>/find_gpu.sh
-# . <PATH PREFIX>/find_gpu.sh 5
-# . <PATH PREFIX>/find_gpu.sh -1
-# . <PATH PREFIX>/find_gpu.sh 3 8000
-# . <PATH PREFIX>/find_gpu.sh -1 20480
+# . find-gpu.sh [#gpu required] [min memory per gpu] [b/w]
 ## Input
 # $1: # of GPU needed
 # $2: lower bound of free memory needed per GPU (in MiB)
@@ -22,7 +18,10 @@ n_gpu_req=${1-"1"}
 # 2nd arg: lower bound of free memory needed per GPU (in MiB), default = 0
 #   i.e. only GPUs with free memory >= $2 will be selected.
 mem_lb=${2-"0"}
-# echo "Requiring ${n_gpu_req} GPUs with at least ${mem_lb}MB free memory"
+# 3rd arg: fit mode, in {b, w}, default = b
+#   b: best fit, prioritises those have LEAST but enough available memory
+#   w: worst fit, prioritises those have MOST and enough available memory
+mode=${3-"b"}
 
 res=$(nvidia-smi | \
 	grep -E "[0-9]+MiB\s*/\s*[0-9]+MiB" | \
@@ -31,8 +30,13 @@ res=$(nvidia-smi | \
 	awk '{print $2 - $1}')
 
 i=0
-res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
-	sort -n -k 2 -r))
+if [ $mode == "b" ]; then
+    res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
+        sort -n -k 2))
+else
+    res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
+        sort -n -k 2 -r))
+fi
 
 if [ ${n_gpu_req} -lt 0 ]; then
 	# echo using all GPUs
@@ -53,8 +57,8 @@ for i in $(seq 0 2 `expr ${#res[@]} - 1`); do
 			gpu_id=${gpu_id}","$gid
 		fi
 		n_gpu_found=`expr 1 + ${n_gpu_found}`
-	else
-		break
+	# else
+	# 	break
 	fi
 done
 
