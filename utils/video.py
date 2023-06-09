4b813be10b2e286a0a752088230bd41889198a75
iTom <44149820+iTomxy@users.noreply.github.com>
2023-06-09 17:11:05 +0800

    upgrade i2v

--- utils/video.py
+++ utils/video.py
@@ -3,25 +3,49 @@ import cv2
 
 
 def i2v(
-    sorted_frame_files, # list of sorted frame files full path
+    sorted_frame_files, # list of str, or list of list of str, full paths to sorted frame files
     dest_video_file,    # str, full path to save the video
     fps=24,
     fourcc="mp4v",
-    overwrite=False     # whether overwrite if `dest_video_file` already exists
+    overwrite=False,    # whether overwrite if `dest_video_file` already exists
+    vstack=False,       # whether stack multiple videos vertically
 ):
-    """concatenate frames into a video"""
+    """concatenate frames into a video
+    sorted_frame_files: List[str] or List[List[str]]
+        If latter, will concatenate the frames within inner list.
+    vstack: bool. If `sorted_frame_files` is List[List[str]],
+        there are multiple videos to be concatenated. By default they
+        will be concatenated horizontally, i.e. `hstack`. If set to true,
+        they will be concatenate vetically.
+    """
     assert overwrite or not osp.isfile(dest_video_file), dest_video_file
     os.makedirs(osp.dirname(dest_video_file) or '.', exist_ok=True)
     writer = None
-    for f in sorted_frame_files:
-        assert osp.isfile(f), f
-        img = cv2.imread(f)
-        assert img is not None, f
+    for fs in sorted_frame_files:
+        if isinstance(fs, str): fs = [fs]
+        img_ls, h, w = [], 0, 0
+        for f in fs:
+            assert osp.isfile(f), f
+            img = cv2.imread(f)
+            assert img is not None, f
+            img_ls.append(img)
+            _h, _w = img.shape[:2]
+            if vstack:
+                h, w = h + _h, max(w, _w)
+            else:
+                h, w = max(h, _h), w + _w
         if writer is None:
-            h, w = img.shape[:2]
             _fourcc = cv2.VideoWriter_fourcc(*fourcc)
             writer = cv2.VideoWriter(dest_video_file, _fourcc, fps, (w, h), True)
-        writer.write(img)
+        cat_frame, offset = np.zeros((h, w, 3), dtype=np.uint8), 0
+        for img in img_ls:
+            if vstack:
+                cat_frame[offset: offset + img.shape[0], :img.shape[1]] = img
+                offset += img.shape[0]
+            else:
+                cat_frame[:img.shape[0], offset: offset + img.shape[1]] = img
+                offset += img.shape[1]
+        writer.write(cat_frame)
     writer.release()
 
 
