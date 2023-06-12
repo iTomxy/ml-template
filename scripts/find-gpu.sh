48e69f4d5f7fc1f16be385f2c7d0dfaacc697219
iTom <44149820+iTomxy@users.noreply.github.com>
2023-06-12 16:08:12 +0800

    find-gpu.sh: robustness in col counting

--- scripts/find-gpu.sh
+++ scripts/find-gpu.sh
@@ -14,47 +14,48 @@
 
 # 1st arg: # of GPU needed, default = 1
 #   <0 means requiring all GPUs.
-n_gpu_req=${1-"1"}
+_n_gpu_req=${1-"1"}
 # 2nd arg: lower bound of free memory needed per GPU (in MiB), default = 0
 #   i.e. only GPUs with free memory >= $2 will be selected.
-mem_lb=${2-"0"}
+_mem_lb=${2-"0"}
 # 3rd arg: fit mode, in {b, w}, default = b
 #   b: best fit, prioritises those have LEAST but enough available memory
 #   w: worst fit, prioritises those have MOST and enough available memory
-mode=${3-"b"}
+_mode=${3-"b"}
 
-res=$(nvidia-smi | \
+_res=$(nvidia-smi | \
 	grep -E "[0-9]+MiB\s*/\s*[0-9]+MiB" | \
-	awk '{print ($9" "$11)}' | \
-	sed "s/\([0-9]\{1,\}\)MiB \([0-9]\{1,\}\)MiB/\1 \2/" | \
+	sed "s/^|//" | \
+	awk '{print ($8" "$10)}' | \
+	sed "s/\([0-9]\+\)MiB \([0-9]\+\)MiB/\1 \2/" | \
 	awk '{print $2 - $1}')
 
-i=0
-if [ $mode == "b" ]; then
-    res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
+_i=0
+if [ $_mode == "b" ]; then
+    _res=($(for _s in $_res; do echo $_i $_s && _i=`expr 1 + $_i`; done | \
         sort -n -k 2))
 else
-    res=($(for s in $res; do echo $i $s && i=`expr 1 + $i`; done | \
+    _res=($(for _s in $_res; do echo $_i $_s && _i=`expr 1 + $_i`; done | \
         sort -n -k 2 -r))
 fi
 
-if [ ${n_gpu_req} -lt 0 ]; then
+if [ ${_n_gpu_req} -lt 0 ]; then
 	# echo using all GPUs
-	n_gpu_req=`expr ${#res[@]} / 2`
+	_n_gpu_req=`expr ${#_res[@]} / 2`
 	# echo ${n_gpu_req}
 fi
 
 gpu_id=-1
 n_gpu_found=0
-for i in $(seq 0 2 `expr ${#res[@]} - 1`); do
-	gid=${res[i]}
-	mem=${res[i+1]}
-	# echo $gid: $mem
-	if [ ${n_gpu_found} -lt ${n_gpu_req} -a $mem -ge ${mem_lb} ]; then
+for i in $(seq 0 2 `expr ${#_res[@]} - 1`); do
+	_gid=${_res[i]}
+	_mem=${_res[i+1]}
+	# echo $_gid: $_mem
+	if [ ${n_gpu_found} -lt ${_n_gpu_req} -a ${_mem} -ge ${_mem_lb} ]; then
 		if [ ${n_gpu_found} -eq 0 ]; then
-			gpu_id=$gid
+			gpu_id=${_gid}
 		else
-			gpu_id=${gpu_id}","$gid
+			gpu_id=${gpu_id}","${_gid}
 		fi
 		n_gpu_found=`expr 1 + ${n_gpu_found}`
 	# else
@@ -63,3 +64,4 @@ for i in $(seq 0 2 `expr ${#res[@]} - 1`); do
 done
 
 # echo found: ${n_gpu_found}
+
