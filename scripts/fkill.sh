7217c01666e1049e3be5749e781ca7144964464d
iTom <tianyou.liang@student.uts.edu.au>
2024-07-07 15:09:41 +1000

    add fkill.sh

---
+++ scripts/fkill.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Kill my processes sieved by the given filter string.
+#   E.g.: [bash] fkill.sh main.py
+# If the filter string is not passed, kill all.
+#   E.g.: [bash] fkill.sh
+
+# filter string
+fs=$1
+
+if [ -z $fs ]; then
+    ps aux | grep `whoami` | awk '{print $2}' | xargs kill -9
+else
+    ps aux | grep `whoami` | grep $fs | awk '{print $2}' | xargs kill -9
+fi
