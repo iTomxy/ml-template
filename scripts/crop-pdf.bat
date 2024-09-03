e571361e788e6f23e4b76545338f8a2f89dd56ab
iTom <tianyou.liang@student.uts.edu.au>
2024-09-03 20:58:07 +1000

    add crop-pdf.bat

---
+++ scripts/crop-pdf.bat
@@ -0,0 +1,24 @@
+@echo off
+@REM Depend on `pdfcrop.exe` provided in texlive. See: https://www.tug.org/texlive/
+@REM Usage:
+@REM     crop-pdf.bat foo.pdf
+
+if (%1) == () (
+	echo * No file specificed
+	exit
+)
+
+for /f %%f in ("%1") do (
+	set "basename=%%~nf"
+	set "p=%%~dpf"
+)
+REM echo %1
+REM echo %basename%
+REM echo %p%
+
+@REM cropped copy saved as `<FILENAME>-crop.pdf`
+pdfcrop "%1"
+@REM delect original pdf
+del /q "%1"
+@REM rename `<FILENAME>-crop.pdf` (back) to <FILENAME>.pdf
+rename "%p%%basename%-crop.pdf" "%basename%.pdf"
